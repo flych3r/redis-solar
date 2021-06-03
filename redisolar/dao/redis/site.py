b8c39322b3766b02bd997e8cee429647598d90d9
flych3r <matheus.sampaio011@gmail.com>
2021-06-03 13:03:20 -0300

    week 1, 2

---
+++ redisolar/dao/redis/site.py
@@ -0,0 +1,46 @@
+from typing import Set
+
+from redisolar.models import Site
+from redisolar.dao.base import SiteDaoBase
+from redisolar.dao.base import SiteNotFound
+from redisolar.dao.redis.base import RedisDaoBase
+from redisolar.schema import FlatSiteSchema
+
+
+class SiteDaoRedis(SiteDaoBase, RedisDaoBase):
+    """SiteDaoRedis persists Site models to Redis.
+
+    This class allows persisting (and querying for) Sites in Redis.
+    """
+    def insert(self, site: Site, **kwargs):
+        """Insert a Site into Redis."""
+        hash_key = self.key_schema.site_hash_key(site.id)
+        site_ids_key = self.key_schema.site_ids_key()
+        client = kwargs.get('pipeline', self.redis)
+        client.hset(hash_key, mapping=FlatSiteSchema().dump(site))
+        client.sadd(site_ids_key, site.id)
+
+    def insert_many(self, *sites: Site, **kwargs) -> None:
+        for site in sites:
+            self.insert(site, **kwargs)
+
+    def find_by_id(self, site_id: int, **kwargs) -> Site:
+        """Find a Site by ID in Redis."""
+        hash_key = self.key_schema.site_hash_key(site_id)
+        site_hash = self.redis.hgetall(hash_key)
+
+        if not site_hash:
+            raise SiteNotFound()
+
+        return FlatSiteSchema().load(site_hash)
+
+    def find_all(self, **kwargs) -> Set[Site]:
+        """Find all Sites in Redis."""
+        # START Challenge #1
+        site_ids_key = self.key_schema.site_ids_key()
+        site_ids = self.redis.smembers(site_ids_key)
+        hash_keys = map(self.key_schema.site_hash_key, site_ids)
+        site_hashes = map(self.redis.hgetall, hash_keys)
+        # END Challenge #1
+
+        return {FlatSiteSchema().load(site_hash) for site_hash in site_hashes}
